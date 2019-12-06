1c29eeb0264b0fb3af6d07f1fea7addf9e6b291f
Kevin <row123321@gmail.com>
2019-12-07 02:35:21 +0800

    加入專案檔案。

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NCUSE12_Taoyuan_Tourism_WebAPP.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
