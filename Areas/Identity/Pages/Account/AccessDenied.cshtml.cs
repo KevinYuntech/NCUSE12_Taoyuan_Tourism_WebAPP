fb6fc9b1877ad813990514467a11034ac3705f5a
Kevin <row123321@gmail.com>
2019-12-09 03:16:49 +0800

    重新覆蓋identity , 更改部分登入頁面為中文

---
+++ Areas/Identity/Pages/Account/AccessDenied.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NCUSE12_Taoyuan_Tourism_WebAPP.Areas.Identity.Pages.Account
+{
+    public class AccessDeniedModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
+
