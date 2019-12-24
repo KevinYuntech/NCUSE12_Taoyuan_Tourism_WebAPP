c73e3907be3c80ce96f87445a3d693643b6a36c3
Kevin <row123321@gmail.com>
2019-12-24 14:50:42 +0800

    新增查詢景點(透過區域碼(仍須改), id)

--- Controllers/Spot/SearchSpot/SearchPublicSpotController.cs
+++ Controllers/Spot/SearchSpot/SearchPublicSpotController.cs
@@ -1,17 +1,38 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using NCUSE12_Taoyuan_Tourism_WebAPP.Data;
 
 namespace NCUSE12_Taoyuan_Tourism_WebAPP.Controllers.Spot.SearchSpot
 {
     public class SearchPublicSpotController : SearchSpotController
     {
-
-        public override IActionResult Index()
+        public SearchPublicSpotController(SpotDbContext context) : base(context)
         {
-            throw new NotImplementedException();
         }
+
+        [HttpGet]
+        public  override IActionResult SearchSpotByDistrict(String zipcode)
+        {
+            //判斷郵遞區號, 回傳該郵遞區號對應景點資料
+            IList result = this._context.PublicSpot.Where(x => x.Name == zipcode).ToList();
+
+            return Json(new { message = result });
+            
+        }
+
+        [HttpGet]
+        public  override IActionResult SearchSpotById(int Id)
+        {
+            //判斷郵遞區號, 回傳該郵遞區號對應景點資料
+            IList result = this._context.PublicSpot.Where(x => x.Id == Id).ToList();
+
+            return Json(new { message = result });
+            
+        }
+
     }
 }
--- Controllers/Spot/SearchSpot/SearchSpotController.cs
+++ Controllers/Spot/SearchSpot/SearchSpotController.cs
@@ -1,13 +1,25 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using NCUSE12_Taoyuan_Tourism_WebAPP.Data;
 
 namespace NCUSE12_Taoyuan_Tourism_WebAPP.Controllers.Spot.SearchSpot
 {
     public abstract class SearchSpotController : Controller
     {
-        public abstract IActionResult Index();
+        protected SpotDbContext _context;
+
+        public SearchSpotController(SpotDbContext context)
+        {
+            this._context = context;
+        }
+        [HttpGet]
+        public  abstract IActionResult SearchSpotByDistrict(String zipcode);
+
+        [HttpGet]
+        public abstract IActionResult SearchSpotById(int id);
     }
 }
