00bdcf56dd6884d8d874dbecd064782f10213ace
Kevin <row123321@gmail.com>
2019-12-25 23:42:09 +0800

    透過session儲存點取行政區 ajax回傳行政區json render表格

--- wwwroot/js/SearchSpotPageByDistrict.js
+++ wwwroot/js/SearchSpotPageByDistrict.js
@@ -5,7 +5,22 @@ $(document).ready(function () {
         data: "",
         dataType: "json",
         success: function (response) {
-            console.log(response.message);
+
+            let data = JSON.parse(response.message);
+            let spotList = data.Data;
+
+            for (let index = 0; index < spotList.length; index++) {
+                let spot_str = "<tr>" +
+                "<td>中壢區</td>"+
+                "<td>" +spotList[index].Name+"</td>" +
+                "<td>" +spotList[index].Address+"</td>" +
+                "<td>" +spotList[index].Description+"</td>" +
+                "<td>" +"<button>新增行程</button>"+"</td>" +
+                "</tr>";
+
+                $('#SpotListTable').append(spot_str);
+            }
+            
         }
     });
 });
