183f543140bbec1570e5fd1428eaf44c690cff8d
Kevin <row123321@gmail.com>
2019-12-27 14:40:50 +0800

    新增景點視窗對應正確欄位(缺照片處理)

--- Models/Spot/PublicSpot.cs
+++ Models/Spot/PublicSpot.cs
@@ -8,21 +8,21 @@ namespace NCUSE12_Taoyuan_Tourism_WebAPP.Models.Spot
         [Key]
         public int Id { get; set; }
 
-        
+        [Required]
         [StringLength(21, ErrorMessage = "Name length can't be more than 50.")]
         public string Name { get; set; }
 
        
-        [StringLength(1165, ErrorMessage = "Address length can't be more than 8.")]
+        [StringLength(1165, ErrorMessage = "Description length can't be more than 8.")]
         public string Description { get; set; } 
 
-        
+        [Required]
         [StringLength(45, ErrorMessage = "Address length can't be more than 8.")]
         public string Address { get; set; }
 
-        
+        [Required]
         public int Zipcode { get; set; } 
-        
+        [Required]
         public String Opentime { get; set; } 
 
         
--- wwwroot/js/SearchSpotPageByDistrict.js
+++ wwwroot/js/SearchSpotPageByDistrict.js
@@ -1,3 +1,4 @@
+//init loading spot list
 $(document).ready(function () {
     $.ajax({
         type: "GET",
@@ -12,7 +13,7 @@ $(document).ready(function () {
             $('#title').text(response.zone);
             for (let index = 0; index < spotList.length; index++) {
                 let spot_str = "<tr>" +
-                "<td>"+spotList[index].Zipcode+"</td>"+
+                "<td>"+spotList[index].Id+"</td>"+
                 "<td>" +spotList[index].Name+"</td>" +
                 "<td>" +spotList[index].Address+"</td>" +
                 "<td>" +spotList[index].Description+"</td>" +
@@ -25,3 +26,109 @@ $(document).ready(function () {
         }
     });
 });
+
+//
+//load current user status
+let userId;
+loadLoginUser();
+console.log(userId);
+
+$(document).ready(function () {
+
+    $('#create_spot_btn').click(function () {
+        $('.addplaceform').animate({
+            height: 'toggle',
+            opacity: 'toggle'
+        }, 'slow');
+    });
+    
+    $('#addplace').click(function (e) { 
+
+        
+        if(userId === null || userId === undefined|| userId === ''){
+            alert('請先登入');
+        }
+        else{
+            console.log("pop a dialog");
+            document.getElementById('addplaceform').style.display = "";
+        }
+    });
+
+    //send spot info to backend
+    $('#submit_info').click(function (e) {
+            let info = {
+                name : $('#placeinput').val(),
+                description : $('#descriptionarea').val(),
+                address : $('#addressinput').val(),
+            }
+            if(name.length && number.length && time.length && address.length&& description.length)
+            {
+                $.ajax({
+                    type: "post",
+                    url: "../CreateSpot/CreateSpotInfo",
+                    data: info,
+                    dataType: "json",
+                    success: function (response) {
+                        //parse json
+                        var returnedData = JSON.parse(response.message);
+    
+                        alert(returnedData.StatusMessage);
+                        console.log(returnedData.Data.ModelStateErrors);
+                    }
+                });
+            }
+            else
+            {
+                alert("尚有未輸入資料!!");
+            } 
+
+    });
+
+    $('#add_itinerary').click(function (e) {
+        console.log('新增行程');
+        let info = {
+            name : $('#placeinput').val(),
+            description : $('#descriptionarea').val(),
+            address : $('#addressinput').val(),
+        }
+        
+        $.ajax({
+            type: "post",
+            url: "/AddItinerary/AddItinerary",
+            data: info,
+            async: false,
+            dataType: "json",
+            success: function (response) {
+                alert(response.status)
+                
+                alert("目前新增數量"+response.message);
+            }
+        });
+});
+});
+
+
+function loadLoginUser()
+{   
+    $(document).ready(function () {
+        $.ajax({
+            type: "get",
+            url: "Account/GetLoginUser",
+            data: "",
+            dataType: "json",
+            async: false,
+            success: function (response) {
+                userId = response.userId;
+                console.log(userId);
+            }
+        });
+    
+    });
+}
+
+
+
+function addplaceclose() {
+    console.log("close a dialog");
+    document.getElementById('addplaceform').style.display = "none";
+}
